d9589eb08944b478cb0be18cbe3629dc7ac892a1
benjaminrayharris <benjaminrayharris@gmail.com>
2020-07-19 13:48:13 -0700

    slight change to structure of site

--- htdocs/specieslist/instructions/instructions.php
+++ htdocs/specieslist/instructions/instructions.php
@@ -14,13 +14,13 @@
                 <p><br/></p><p><br/></p><h2 class="notes">Species List Program instructions...</h2>
                 <p class="info-text">Your IP address '<?php echo $ip; ?>' is assigned to the <?php echo $api_result['city']; ?> area</p>
                 <p class="notes">On visiting this page, your computer sent this identification string:<br/>'<?php echo("".$_SERVER['HTTP_USER_AGENT'].""); ?>'<br/> from which we think you are using <?php echo $device_details; ?></p><p><br/></p>
-                <p class="notes"><a href="/specieslist/instructions/dandi.php">Download and Install</a></p>
-                <p class="notes"><a href="/specieslist/instructions/masterlist.php">Initial Masterlist File</a></p>
-                <p class="notes"><a href="/specieslist/instructions/shortcut.php">Make a Shortcut Link</a></p>
-                <p class="notes"><a href="/specieslist/instructions/macro.php">Using the Excel Macro</a></p>
-                <p class="notes"><a href="/specieslist/instructions/firstrun.php">First Time Use</a></p>
-                <p class="notes"><a href="/specieslist/instructions/general.php">General Use</a></p>
-                <p class="notes"><a href="/specieslist/instructions/update.php">Update Program</a></p>
+                <p class="notes"><a href="/specieslist/instructions/dandi">Download and Install</a></p>
+                <p class="notes"><a href="/specieslist/instructions/masterlist">Initial Masterlist File</a></p>
+                <p class="notes"><a href="/specieslist/instructions/shortcut">Make a Shortcut Link</a></p>
+                <p class="notes"><a href="/specieslist/instructions/macro">Using the Excel Macro</a></p>
+                <p class="notes"><a href="/specieslist/instructions/firstrun">First Time Use</a></p>
+                <p class="notes"><a href="/specieslist/instructions/general">General Use</a></p>
+                <p class="notes"><a href="/specieslist/instructions/update">Update Program</a></p>
                 <p><br/></p><div class="subscribe"></div>
             </div>
         </div>
